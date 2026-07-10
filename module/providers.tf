0bdeaf8d59c1e2614f86a5d96ce8159319930a0a
Cássio Moreira <cassioliveiram@gmail.com>
2026-07-10 14:44:01 -0300

    comment providers definition
    Signed-off-by: Cássio Moreira <cassioliveiram@gmail.com>

--- module/providers.tf
+++ module/providers.tf
@@ -1,14 +1,14 @@
-terraform {
-  required_version = ">= 1.5.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 6.28.0, < 7.0.0" # CREATE_ON_PUSH em ecr_repository_creation_template só existe a partir da 6.28.0
-    }
-  }
-}
-
-provider "aws" {
-  region = var.aws_region
-}
+# terraform {
+#   required_version = ">= 1.5.0"
+#
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = ">= 6.28.0, < 7.0.0" # CREATE_ON_PUSH em ecr_repository_creation_template só existe a partir da 6.28.0
+#     }
+#   }
+# }
+#
+# provider "aws" {
+#   region = var.aws_region
+# }
